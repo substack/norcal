3665384226e54dec0480696e054c468af43e5a8b
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-04-28 16:49:56 -1000

    factored out trie and show-month into lib/ files

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,26 +4,13 @@ var minimist = require('minimist')
 var path = require('path')
 var homedir = require('os-homedir')
 var norcal = require('../')
+var showMonth = require('../lib/show-month.js')
 var hyperlog = require('hyperlog')
 var level = require('level')
 var mkdirp = require('mkdirp')
-var calmonth = require('calendar-month-string')
-var layers = require('text-layers')
-var fcolor = require('fuzzy-ansi-color')
-var strftime = require('strftime')
 var sprintf = require('sprintf')
-var has = require('has')
 var fs = require('fs')
 var path = require('path')
-var uniq = require('uniq')
-
-var reset = fcolor('reset')
-var soft = '\x1b[27m'
-
-var xcolors = [
-  'cyan', 'lime', 'orange', 'magenta',
-  'yellow', 'red', 'blue', 'violet'
-]
 
 var argv = minimist(process.argv.slice(2), {
   alias: {
@@ -119,69 +106,10 @@ if (argv._[0] === 'add') {
     })
     var today = new Date
     Object.keys(mdocs).sort().forEach(function (ym) {
-      showMonth(new Date(ym), today, mdocs[ym])
+      console.log(showMonth(new Date(ym), today, mdocs[ym]))
       console.log()
     })
   })
-}
-
-function showMonth (date, today, docs) {
-  var colors = {}
-  var titles = {}
-  var times = {}
-  var index = 0
-  var indexes = {}
-  var keys = []
-
-  // Determine the minimum key substring to show
-  var shortKeys = abbreviate(uniq(Object.keys(docs).map(function (name) {
-    return docs[name].key
-  })))
-  Object.keys(shortKeys).forEach(function (name, idx) {
-    docs[idx].shortKey = shortKeys[name]
-  })
-
-  // Accumulate all event keys
-  docs.forEach(function (doc) {
-    if (!has(indexes,doc.key)) {
-      indexes[doc.key] = index++
-      keys.push({
-        key: doc.key,
-        day: Number(strftime('%e', doc.time))
-      })
-    }
-  })
-
-  function colorForDate (today, date) {
-    var day = strftime('%w', date)
-    var colour = 'bright ' + xcolors[day % xcolors.length]
-    if (strftime('%F',today) === strftime('%F',date)) {
-      return 'reverse'
-    } else {
-      return 'bright ' + colour
-    }
-  }
-
-  if (strftime('%Y-%m',today) === strftime('%Y-%m',date)) {
-    colors[today.getDate()] = 'reverse'
-  }
-  docs.forEach(function (doc) {
-    var d = Number(strftime('%e', doc.time))
-    var i = indexes[doc.key]
-    colors[d] = colorForDate(today, doc.time)
-    titles[doc.key] = doc.value.title
-    times[doc.key] = strftime('%a %b %e %H:%M', doc.time)
-  })
-  var caltxt = calmonth(date, { colors: colors })
-  var evlines = docs.map(function (doc) {
-    var c = colorForDate(today, doc.time)
-    return fcolor(c) + '[' + doc.shortKey + '] '
-      + times[doc.key] + reset + ' ' + titles[doc.key]
-  })
-  console.log(layers([
-    { text: caltxt, x: 0, y: 0 },
-    { text: evlines.join('\n'), x: 22, y: 1 }
-  ]))
 }
 
 function getDocFromAbbreviation (key, cb) {
@@ -226,47 +154,3 @@ function usage (code) {
   if (code) r.once('end', function () { process.exit(code) })
   r.pipe(process.stdout)
 }
-
-function abbreviate (lst) {
-  var trie = buildTrie(lst)
-
-  var res = {}
-
-  lst.forEach(function (elm) {
-    var t = trie
-    for (var i=0; i < elm.length; i++) {
-      var hasNext = Object.keys(t[elm[i]]).length > 0
-      if (i === elm.length - 1 || !hasNext) {
-        res[elm] = elm
-        break
-      } else if (t[elm[i]].idx === 0) {
-        res[elm] = elm.substring(0, i + 1)
-        break
-      } else {
-        t = t[elm[i]]
-      }
-    }
-  })
-
-  return res
-
-  function buildTrie (lst) {
-    var trie = {}
-    for (var i=0; i < lst.length; i++) {
-      var key = lst[i]
-      var t = trie
-      for (var j=0; j < key.length; j++) {
-        var ch = key[j]
-        if (t[ch] === undefined) {
-          t[ch] = { idx: 0 }
-          t = t[ch]
-        } else {
-          t = t[ch]
-          t.idx++
-        }
-      }
-    }
-    return trie
-  }
-}
-
